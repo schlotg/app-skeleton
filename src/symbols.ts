413eaa4473f907459f5c670226b0c78a0145ee0b
Greg Schlottmann <schlotg123@gmail.com>
2019-09-14 15:54:24 -0700

    initial commit

---
+++ src/symbols.ts
@@ -0,0 +1,7 @@
+export const Symbols = {
+  // components
+  Component: Symbol.for('Component'),
+  AppComponent: Symbol.for('AppComponent'),
+  // services
+  RouterService: Symbol.for('RouterService'),
+}
---
+++ types.d.ts
@@ -0,0 +1,8 @@
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.svg";
+declare module "*.gif";
+declare module "*.html";
+declare const NAME: string;
+declare const VERSION: string;
+declare const PRODUCTION: boolean;
